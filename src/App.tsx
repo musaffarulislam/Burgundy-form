c8749efd152f0430806997995943f962e04254a4
musaffarukislam <musaffarulislam@gmail.com>
2024-04-25 01:45:03 +0530

    feat: delete building error and remove unwanted files and code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import Form from "./components/Form"
-import Form2 from "./components/Form2"
 function App() {
 
   return (
     <div className="h-screen flex justify-center">
       <div className="w-full h-fit flex flex-col items-center max-w-[600px] p-8">
         {/* <Form /> */}
-        <Form2 />
+        <Form />
       </div>
     </div>
   )
